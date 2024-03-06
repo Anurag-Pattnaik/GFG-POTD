4026d982d772f32cf2df5c08dc937ccd67043550
Anurag-Pattnaik <159184422+Anurag-Pattnaik@users.noreply.github.com>
2024-03-06 23:39:06 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> search(String pattern, String text)
+    {
+        // your code here
+        
+        ArrayList<Integer>list=new ArrayList<>();
+        int pos=0;
+        
+        while(pos!=-1)
+        {
+            pos=text.indexOf(pattern,pos);
+            if(pos!=-1)
+            {
+                list.add(pos+1);
+                pos=pos+1;
+            }
+        }
+        return list;
+    }
+}
